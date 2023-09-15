08edaa1d8f8dddf3627f639777a33a4d8182209d
Alex Merced <alex@alexmerced.dev>
2023-09-15 09:01:06 -0400

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,28 @@
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "Class Name",
+      social: {
+        github: "https://github.com/AlexMercedCoder/starlight-class-template/",
+      },
+      sidebar: [
+        {
+          label: "Curriculum",
+          autogenerate: { directory: "curriculum" },
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+        {
+          label: "Other",
+          autogenerate: { directory: "other" },
+        },
+      ],
+    }),
+  ],
+});
